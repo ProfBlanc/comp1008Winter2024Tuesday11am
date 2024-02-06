c69b0823183279e4e6e55ea372048a7727602955
Prof Blanc <ben.blanc@georgebrown.ca>
2024-02-06 12:07:14 -0500

    Class Code feb 6, 2024 @ 12h10

---
+++ src/wk5/Week5.java
@@ -0,0 +1,13 @@
+package wk5;
+
+public class Week5 {
+
+    //psvm+tab
+    public static void main(String[] args) {
+
+
+        PizzaOrder pizzaOrder = new PizzaOrder();
+        pizzaOrder.getPrice();
+        String toppings = pizzaOrder.toppings;
+    }
+}
